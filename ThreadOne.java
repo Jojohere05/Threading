9526209ffa8e112373fee9ab2e2c3e4af853387d
Jojohere05 <2005jhotikaraja@gmail.com>
2025-04-15 23:43:01 +0530

    ThreadOne comments

--- ThreadOne.java
+++ ThreadOne.java
@@ -1,7 +1,9 @@
+// This class demonstrates a thread that prints numbers 1 to 5
 public class ThreadOne extends Thread {
     public void run() {
         printNumbers();
     }
+    // Function to print numbers from 1 to 5
     public void printNumbers() {
         for (int i = 1; i <= 5; i++) {
             System.out.println("Thread One - Number: " + i);
