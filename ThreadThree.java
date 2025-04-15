cb6098d09ad44ee33e0a4501c277041f90e1d22c
Jojohere05 <2005jhotikaraja@gmail.com>
2025-04-16 00:19:05 +0530

    method printSquare

--- ThreadThree.java
+++ ThreadThree.java
@@ -3,6 +3,13 @@ public class ThreadThree {
         printSquares();
     }
     public void printSquares() {
-        
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread Three - Square: " + (i * i));
+            try {
+                Thread.sleep(600); // pause for 600 ms
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted.");
+            }
+        }
     }
 }
