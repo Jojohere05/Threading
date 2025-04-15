ae62a466898e80f8d84791440b29450d78cf3cf2
Jojohere05 <2005jhotikaraja@gmail.com>
2025-04-16 00:22:09 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,5 +4,15 @@ PRN: 23070126050
 Batch: A2
 */
 public class Main {
-    
+    public static void main(String[] args) {
+        // Create instances of different threads
+        ThreadOne t1 = new ThreadOne();
+        ThreadTwo t2 = new ThreadTwo();
+        ThreadThree t3 = new ThreadThree();
+
+        // Start threads
+        t1.start();
+        t2.start();
+        t3.start();
+    }
 }
