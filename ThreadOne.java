17f59b68113b7359cc826384a05a7f8a8191c59f
Jojohere05 <2005jhotikaraja@gmail.com>
2025-04-15 23:42:22 +0530

    printNumber implementation

--- ThreadOne.java
+++ ThreadOne.java
@@ -3,5 +3,13 @@ public class ThreadOne extends Thread {
         printNumbers();
     }
     public void printNumbers() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread One - Number: " + i);
+            try {
+                Thread.sleep(500); // pause for 500 ms
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted.");
+            }
+        }
     }
 }
