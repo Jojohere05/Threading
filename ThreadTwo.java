80f8ab162d47eb4bafed7fa357a86a381fd29a88
Jojohere05 <2005jhotikaraja@gmail.com>
2025-04-15 23:51:08 +0530

    method

--- ThreadTwo.java
+++ ThreadTwo.java
@@ -3,5 +3,13 @@ public class ThreadTwo extends Thread {
         printCharacters();
     }
     public void printCharacters() {
+        for (char ch = 'A'; ch <= 'E'; ch++) {
+            System.out.println("Thread Two - Character: " + ch);
+            try {
+                Thread.sleep(700); // pause for 700 ms
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted.");
+            }
+        }
     }
 }
