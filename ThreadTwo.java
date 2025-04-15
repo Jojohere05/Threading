aa0c50df65d8cdb1bed0fb04cb640906b79c26fa
Jojohere05 <2005jhotikaraja@gmail.com>
2025-04-16 00:17:10 +0530

    cooments

--- ThreadTwo.java
+++ ThreadTwo.java
@@ -1,7 +1,9 @@
+// This class demonstrates a thread that prints characters A to E
 public class ThreadTwo extends Thread {
     public void run() {
         printCharacters();
     }
+    // Function to print characters from A to E
     public void printCharacters() {
         for (char ch = 'A'; ch <= 'E'; ch++) {
             System.out.println("Thread Two - Character: " + ch);
